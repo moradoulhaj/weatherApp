60b26224eaf014ababcdceeb15cfe3e6f4755a60
moradoulhaj <moradoulhaj.off@gmail.com>
2024-06-14 01:11:03 +0100

    making icons dynamic

--- Weather App/src/components/Weather.jsx
+++ Weather App/src/components/Weather.jsx
@@ -10,52 +10,85 @@ import wind_icon from "../assets/wind.png";
 import "./Weather.css";
 function Weather() {
   const [weatherData, setWeatherData] = useState(false);
-  const [errorr, setErrorr] = useState("");
   const inputCountryRef = useRef();
+  const allIcons = {
+    "01n.png": clear_icon,
+    "01d.png": clear_icon,
+    "02d.png": clear_icon,
+    "02n.png": clear_icon,
+    "03d.png": cloud_icon,
+    "03n.png": cloud_icon,
+    "04d.png": cloud_icon,
+    "04n.png": cloud_icon,
+    "09d.png": rain_icon,
+    "09n.png": rain_icon,
+    "10d.png": rain_icon,
+    "10n.png": rain_icon,
+    "11d.png": drizzle_icon,
+    "11n.png": drizzle_icon,
+    "13d.png": snow_icon,
+    "13n.png": snow_icon,
+    "50d.png": snow_icon,
+    "50n.png": snow_icon,
+  };
   const search = async (city) => {
-    if (city == "") { alert("Would you enter city name!"); return 0; }
+    if (city == "") {
+      alert("Would you enter city name!");
+      return 0;
+    }
 
-    
     // let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=${Api}`;
     try {
-      let Api = '4c70cc9610b6ce35762a9f45ba96b62a';
-      let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=${Api}`);
+      let Api = "4c70cc9610b6ce35762a9f45ba96b62a";
+      let response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=${Api}`
+      );
       let data = await response.json();
-       setWeatherData({
+      const icon = allIcons[data.weather[0].icon + ".png"] || clear_icon;
+      setWeatherData({
         temperature: data.main.temp,
         humidity: data.main.humidity,
         wind: data.wind.speed,
         name: data.name,
-        icon: data.weather[0].icon
-      })
-    } catch (error) {
-
-    }
-    
-
-
-  }
+        icon: icon,
+      });
+      console.log(data.weather[0].icon);
+      console.log(allIcons[data.weather[0].icon]);
+      // await console.log(allIcons[data.weather[0].icon]);
+    } catch (error) {}
+  };
   return (
     <div className="weather">
       <div className="search-bar">
-        <input type="text" name="" id="" placeholder="Search" className="cityInput" ref={inputCountryRef}/>
-        <img src={search_icon} alt="" onClick={()=>search(inputCountryRef.current.value)} />
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Search"
+          className="cityInput"
+          ref={inputCountryRef}
+        />
+        <img
+          src={search_icon}
+          alt=""
+          onClick={() => search(inputCountryRef.current.value)}
+        />
       </div>
       <div>
-        <img src={clear_icon} alt="" className="weather-icon" />
+        <img src={weatherData.icon} alt="" className="weather-icon" />
         <p className="temp">{weatherData.temperature}°C</p>
         <p className="location">{weatherData.name}</p>
       </div>
       <div className="weather-data">
         <div className="col">
-          <img src={humidity_icon} alt=""  />
+          <img src={humidity_icon} alt="" />
           <div>
             <p>{weatherData.humidity} %</p>
             <span>Humidity</span>
           </div>
         </div>
         <div className="col">
-          <img src={wind_icon} alt=""  />
+          <img src={wind_icon} alt="" />
           <div>
             <p>{weatherData.wind} km/h</p>
             <span>Wind Speed</span>
